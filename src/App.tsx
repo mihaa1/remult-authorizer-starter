b3d5d65cd3a6d6e828e9a559f1250f47de79eb2c
spiderman <spiderman@Michaels-MacBook-Pro.local>
2024-04-06 19:05:18 +0300

    wip

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,100 @@
-import { useEffect, useState } from 'react'
+// import { useEffect, useState } from 'react'
 // import reactLogo from './assets/react.svg'
 // import viteLogo from '/vite.svg'
 import './App.css'
-import { remult } from 'remult'
-import { User } from './shared/models/User'
+// import { remult } from 'remult'
+// import { User } from './shared/models/User'
+import {
+	AuthorizerProvider,
+	Authorizer,
+	useAuthorizer,
+} from '@authorizerdev/authorizer-react'
+// import { Authorizer as AuthorizerJS } from '@authorizerdev/authorizer-js'
 
 function App() {
-	const [users, setUsers] = useState<User[]>([])
+	// const [users, setUsers] = useState<User[]>([])
 
-	useEffect(() => {
-		fetchUsers()
-	}, [])
+	// useEffect(() => {
+	// 	fetchUsers()
+	// }, [])
 
-	const fetchUsers = async () => {
-		try {
-			const res = await remult.repo(User).find()
-			setUsers(res)
-		} catch (e) {
-			console.log('Error fetching users', e)
-		}
+	// const fetchUsers = async () => {
+	// 	try {
+	// 		const res = await remult.repo(User).find()
+	// 		setUsers(res)
+	// 	} catch (e) {
+	// 		console.log('Error fetching users', e)
+	// 	}
+	// }
+	const testReq = async () => {
+		await fetch('/api/hello')
 	}
 
 	return (
-		<>
-			{users.map((u) => (
-				<div>{u.email}</div>
-			))}
-		</>
+		<AuthorizerProvider
+			onStateChangeCallback={async ({ token }) => {
+				console.log('token', token)
+				if (!token) {
+					return console.log('logged out...')
+				}
+				console.log('calling /api/current_user')
+				// await fetch('/api/current_user', {
+				// 	method: 'POST',
+				// 	headers: {
+				// 		'Content-Type': 'application/json',
+				// 	},
+				// 	body: JSON.stringify(token),
+				// })
+				// const authorizerJSRef = new AuthorizerJS({
+				// 	authorizerURL: 'http://localhost:8080',
+				// 	redirectURL: window.location.origin,
+				// 	// clientID: 'b0edf65f-3d16-4ed8-af0b-45a788709752',
+				// 	clientID: 'b04ce106-f733-443b-8e82-412bb44aecb6',
+				// })
+				// const session = await authorizerJSRef.validateSession()
+				// console.log('token.access_token', token.access_token)
+				// const profile = await authorizerJSRef.getProfile({
+				// 	Authorization: `Bearer ${token.access_token}`,
+				// })
+				// console.log('>>>> session', session)
+				// console.log('>>>> profile', profile)
+			}}
+			config={{
+				// authorizerURL: 'https://authorizer-production-734e.up.railway.app',
+				// authorizerURL: 'http://localhost:8080',
+				// redirectURL: window.location.origin,
+				// clientID: 'b0edf65f-3d16-4ed8-af0b-45a788709752', // obtain your client id from authorizer dashboard
+				// extraHeaders: {}, // Optional JSON object to pass extra headers in each authorizer requests.
+				authorizerURL: import.meta.env.VITE_AUTHORIZER_URL,
+				redirectURL: window.location.origin,
+				clientID: import.meta.env.VITE_AUTHORIZER_CLIENT_ID,
+			}}
+		>
+			<LoginSignup />
+			<Profile />
+			<button onClick={testReq}>Fire</button>
+		</AuthorizerProvider>
 	)
+}
+
+const LoginSignup = () => {
+	const { user } = useAuthorizer()
+
+	if (user) {
+		return null
+	}
+
+	return <Authorizer />
+}
+
+const Profile = () => {
+	const { user } = useAuthorizer()
+
+	if (user) {
+		return <div>Logged in :) Profile: {user.email}</div>
+	}
+
+	return null
 }
 
 export default App
--- src/server/index.ts
+++ src/server/index.ts
@@ -1,14 +1,84 @@
+import 'dotenv/config'
 import express, { NextFunction, Request, Response } from 'express'
 import { api } from './api'
+import { Authorizer } from '@authorizerdev/authorizer-js'
+import bodyParser from 'body-parser'
+import cookieParser from 'cookie-parser'
+import path from 'path'
+
 const app = express()
 
-app.get('/hello', (_req, res) => res.send('Hello World!'))
+app.use(cookieParser())
+
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }))
+// parse application/json
+app.use(bodyParser.json())
+
+app.use(express.static(path.join(__dirname, '../', '../', 'dist')))
+
+// real auth
+app.use(async (req: Request, res: Response, next: NextFunction) => {
+	const authorization = req.cookies['cookie_session']
+	console.log('authorization', authorization)
+	const authorizerRef = new Authorizer({
+		// authorizerURL: 'http://localhost:8080',
+		// redirectURL: 'http://localhost:3002',
+		// // clientID: 'b0edf65f-3d16-4ed8-af0b-45a788709752',
+		// clientID: 'b04ce106-f733-443b-8e82-412bb44aecb6',
+		authorizerURL: process.env.VITE_AUTHORIZER_URL || '',
+		redirectURL: process.env.AUTHORIZER_REDIRECT_URL || '',
+		clientID: process.env.VITE_AUTHORIZER_CLIENT_ID || '',
+	})
+	const session = await authorizerRef.validateSession({
+		cookie: authorization,
+	})
+	console.log('>>>> session', session)
+	if (session) {
+		return next()
+	}
+	res.status(403).send('Unauthorized MW')
+})
+
+app.get('/api/hello', (req, res) => {
+	console.log('req.cookies', req.cookies)
+	res.send('Hello World!')
+})
+
+// app.post('/api/current_user', (req, res) => {
+// 	if (!req.body) {
+// 		return res.status(401).json({
+// 			message: 'Unauthorized',
+// 		})
+// 	}
+// 	const data = req.body
+// 	const accessToken = data?.access_token
+// 	const expiresAt = data?.expires_in
+
+// 	if (!accessToken || !expiresAt) {
+// 		return res.status(401).json({
+// 			message: 'Unauthorized',
+// 		})
+// 	}
+// 	const now = new Date()
+// 	now.setSeconds(now.getSeconds() + expiresAt)
+// 	res.setHeader(
+// 		'Set-Cookie',
+// 		`authorizer_client=${accessToken};Expires=${now.toUTCString()};Secure=true;HttpOnly=true;Path=/`
+// 	)
+// 	res.status(200).json({ message: 'session created successfully' })
+// })
 
 // fake auth middleware
-app.use((req: Request, _res: Response, next: NextFunction) => {
-	req.user = { ...req.user, id: '123', email: 'superman@marvel.com' }
-	next()
-})
+// app.use((req: Request, res: Response, next: NextFunction) => {
+// 	// req.user = { ...req.user, id: '123', email: 'superman@marvel.com' }
+// 	if (req.user) {
+// 		return next()
+// 	}
+// 	res.status(403).send('Unauthorized')
+// })
+
+app.get('/hello-authenticated', (_req, res) => res.send('/hello-authenticated'))
 
 app.use(api)
 
