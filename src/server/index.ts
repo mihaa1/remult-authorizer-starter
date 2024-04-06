0606222a31bdf08a17833fd7364f085934b29ca2
spiderman <spiderman@Michaels-MacBook-Pro.local>
2024-04-06 21:23:22 +0300

    cleanup

--- src/server/index.ts
+++ src/server/index.ts
@@ -20,13 +20,7 @@ app.use(express.static(path.join(__dirname, '../', '../', 'dist')))
 // real auth
 app.use(async (req: Request, res: Response, next: NextFunction) => {
 	const authorization = req.cookies['cookie_session_domain']
-	console.log('authorization', authorization)
-	console.log('req.cookies', req.cookies)
 	const authorizerRef = new Authorizer({
-		// authorizerURL: 'http://localhost:8080',
-		// redirectURL: 'http://localhost:3002',
-		// // clientID: 'b0edf65f-3d16-4ed8-af0b-45a788709752',
-		// clientID: 'b04ce106-f733-443b-8e82-412bb44aecb6',
 		authorizerURL: process.env.VITE_AUTHORIZER_URL || '',
 		redirectURL: process.env.AUTHORIZER_REDIRECT_URL || '',
 		clientID: process.env.VITE_AUTHORIZER_CLIENT_ID || '',
@@ -34,7 +28,6 @@ app.use(async (req: Request, res: Response, next: NextFunction) => {
 	const session = await authorizerRef.validateSession({
 		cookie: authorization,
 	})
-	console.log('>>>> session.data', session?.data)
 	session.data
 	if (session) {
 		return next()
@@ -42,43 +35,7 @@ app.use(async (req: Request, res: Response, next: NextFunction) => {
 	res.status(403).send('Unauthorized MW')
 })
 
-app.get('/api/hello', (_req, res) => {
-	// console.log('req.cookies', req.cookies)
-	res.send('Hello World!')
-})
-
-// app.post('/api/current_user', (req, res) => {
-// 	if (!req.body) {
-// 		return res.status(401).json({
-// 			message: 'Unauthorized',
-// 		})
-// 	}
-// 	const data = req.body
-// 	const accessToken = data?.access_token
-// 	const expiresAt = data?.expires_in
-
-// 	if (!accessToken || !expiresAt) {
-// 		return res.status(401).json({
-// 			message: 'Unauthorized',
-// 		})
-// 	}
-// 	const now = new Date()
-// 	now.setSeconds(now.getSeconds() + expiresAt)
-// 	res.setHeader(
-// 		'Set-Cookie',
-// 		`authorizer_client=${accessToken};Expires=${now.toUTCString()};Secure=true;HttpOnly=true;Path=/`
-// 	)
-// 	res.status(200).json({ message: 'session created successfully' })
-// })
-
-// fake auth middleware
-// app.use((req: Request, res: Response, next: NextFunction) => {
-// 	// req.user = { ...req.user, id: '123', email: 'superman@marvel.com' }
-// 	if (req.user) {
-// 		return next()
-// 	}
-// 	res.status(403).send('Unauthorized')
-// })
+app.get('/api/hello', (_req, res) => res.send('Hello World!'))
 
 app.get('/hello-authenticated', (_req, res) => res.send('/hello-authenticated'))
 
